5354db69ec13bd3ab5f750cdefcd9e49d14cf0a2
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-14 11:02:18 +0300

    OBG-002: Guess the flag project created

---
+++ Guess the Flag/Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Ozan Barış Günaydın on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
