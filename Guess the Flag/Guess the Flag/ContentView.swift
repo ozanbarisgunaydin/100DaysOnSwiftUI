560918c4d3428fc11f96c82eea274cebc6096df0
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-14 14:40:49 +0300

    OBG-002: Alert views

--- Guess the Flag/Guess the Flag/ContentView.swift
+++ Guess the Flag/Guess the Flag/ContentView.swift
@@ -111,3 +111,20 @@ struct ContentView: View {
 //        .cornerRadius(4)
 //}
 
+
+// MARK: - Simple Alert
+//struct ContentView: View {
+//    @State private var showingAlert = false
+//    
+//    var body: some View {
+//        Button("Show Alert!") {
+//            showingAlert = true
+//        }
+//        .alert("Important message", isPresented: $showingAlert) {
+//            Button("Delete", role: .destructive) { }
+//            Button("Cancel", role: .cancel) { }
+//        } message: {
+//            Text("Please read this.")
+//        }
+//    }
+//}
