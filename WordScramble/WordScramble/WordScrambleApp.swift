697ac93bdce66d315d2d3957b478961e9e28b132
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-25 15:32:44 +0300

    OBG-004: World scramble project added

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ozan Barış Günaydın on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
