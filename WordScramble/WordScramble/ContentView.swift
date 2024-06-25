2ad2e62cb3b50651b892f9b731f533502d297146
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-25 17:43:35 +0300

    OBG-004: Project completed

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,27 +11,58 @@ struct ContentView: View {
     @State private var usedWords: [String] = []
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var score = 0
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
 
     var body: some View {
-        NavigationStack {
-            List {
-                Section {
-                    TextField("Enter your word", text: $newWord)
-                        .textInputAutocapitalization(.never)
-                }
-                
-                Section {
-                    ForEach(usedWords, id: \.self) { word in
-                        HStack {
-                            Image(systemName: "\(word.count).circle")
-                            Text(word)
+        VStack {
+            NavigationStack {
+                List {
+                    Section {
+                        TextField("Enter your word", text: $newWord)
+                            .textInputAutocapitalization(.never)
+                    }
+                    
+                    Section {
+                        ForEach(usedWords, id: \.self) { word in
+                            HStack {
+                                Image(systemName: "\(word.count).circle")
+                                Text(word)
+                            }
                         }
                     }
                 }
+                .navigationTitle(rootWord)
+                .toolbar {
+                    Button("New Word!", action: startGame)
+                }
+                .onSubmit(addNewWord)
+                .onAppear(perform: startGame)
+                .alert(errorTitle, isPresented: $showingError) { } message: {
+                    Text(errorMessage)
+                }
             }
-            .navigationTitle(rootWord)
-            .onSubmit(addNewWord)
-            .onAppear(perform: startGame)
+            Spacer()
+            ZStack {
+                LinearGradient(
+                    colors: [.red.opacity(0.4), .blue.opacity(0.8)],
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                    .cornerRadius(20)
+                VStack {
+                    Text("Total Score")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                    Text("\(score)")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                }
+            }
+            .padding(16)
         }
     }
     
@@ -40,8 +71,33 @@ struct ContentView: View {
         
         guard answer.count > 0 else { return }
         
+        guard isEnoughLength(word: answer) else {
+            wordError(title: "Word not enough length", messsage: "The word must be more than 2 letters!")
+            return
+        }
+        
+        guard isDifferentFromStartWord(word: answer) else {
+            wordError(title: "Word not computed", messsage: "Write different then the root word!")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", messsage: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", messsage: "You cannot spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", messsage: "You cannot just make them up!")
+            return
+        }
         
         withAnimation {
+            score += answer.count
             usedWords.insert(answer, at: 0)
             newWord = ""
         }
@@ -55,6 +111,56 @@ struct ContentView: View {
         
         let allWords = startWords.components(separatedBy: "\n")
         rootWord = allWords.randomElement() ?? "silkworm"
+        
+        usedWords = []
+        score = 0
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            guard let pos = tempWord.firstIndex(of: letter ) else {
+                return false
+            }
+            tempWord.remove(at: pos)
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(
+            location: 0,
+            length: word.utf16.count
+        )
+        let misspelledRange = checker.rangeOfMisspelledWord(
+            in: word,
+            range: range,
+            startingAt: 0,
+            wrap: false,
+            language: "en"
+        )
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func isEnoughLength(word: String) -> Bool {
+        word.count > 2
+    }
+    
+    func isDifferentFromStartWord(word: String) -> Bool {
+        word != rootWord
+    }
+    
+    func wordError(title: String, messsage: String) {
+        errorTitle = title
+        errorMessage = messsage
+        showingError = true
     }
 }
 
