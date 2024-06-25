b253423d2f8f5ebf1407638477928769751d6467
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-25 10:42:28 +0300

    OBG-004: Better Rest app added
    with basic stepper

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ozan Barış Günaydın on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Ozan Barış Günaydın on 25.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper(
+            "\(sleepAmount.formatted()) hours",
+            value: $sleepAmount,
+            in: 4...12,
+            step: 0.25
+        )
+    }
+}
+
+#Preview {
+    ContentView()
+}
