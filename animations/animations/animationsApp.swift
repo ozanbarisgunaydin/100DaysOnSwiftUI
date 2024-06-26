42b3f1e4c2c0221f476ff70128c8974f9a36b9db
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-26 17:20:15 +0300

    OBG-005: Animations project created

---
+++ animations/animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Ozan Barış Günaydın on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
