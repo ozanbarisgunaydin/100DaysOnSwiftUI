f54794f7bd4f6083776286eb601f4244c2be65f0
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-27 13:56:42 +0300

    OBG-006:  show & hide a second view

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
+struct SecondView: View {
+    @Environment(\.dismiss) private var dismiss
+    let name: String
+    
+    var body: some View {
+        VStack {
+            Text("Hello, \(name)")
+            Button("Dismiss") {
+                dismiss()
+            }
+            
+        }
+        .padding()
+    }
+}
+
 struct ContentView: View {
     @State private var showingSheet = false
     
     var body: some View {
         Button("Show Sheet") {
             showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "OBG")
         }
     }
 }
