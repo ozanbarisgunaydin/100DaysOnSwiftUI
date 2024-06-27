e1f70a57626c2f91a27804465c7f210e065f12c5
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-27 16:03:31 +0300

    OBG-006: Identifiable protocol

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,13 +7,41 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable { /// `Identifiable` guarantee the uniq identity and we can delete the `id: \.id` line on the `ForEach`
+    let id = UUID() /// This is required from Identifiable protocol
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable /// Makes updates views
+class Expenses {
+    var items: [ExpenseItem] = []
+}
+
 struct ContentView: View {
-    @AppStorage("tapCount") private var tapCount = 0
+    @State private var expenses = Expenses()
     
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount += 1
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
