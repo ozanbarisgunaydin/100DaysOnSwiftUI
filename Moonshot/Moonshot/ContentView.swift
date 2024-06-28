e88828ca2e48aa6c86ec71edfd3364e88bcb8ff1
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-28 11:03:29 +0300

    OBG-007: Navigation Link

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,28 +7,20 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(text: String) {
-        print("Creating a new Custom Text")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 10) { /// Loads items if needed with `Lazy` but the size expands and it may create some UI issues
-                ForEach(0..<100) {
-                    CustomText(text: "Item of \($0)")
-                        .font(.title)
+        NavigationStack {
+            NavigationLink {
+                Text("Detail View")
+            } label: {
+                VStack {
+                    Text("This is the label")
+                    Text("So is this")
+                    Image(systemName: "face.smiling")
                 }
+                .font(.largeTitle)
             }
+            .navigationTitle("SwiftUI")
         }
     }
 }
