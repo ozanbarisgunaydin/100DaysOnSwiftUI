ba670bf74b6e82be21b2effbfc251651e266dd38
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-28 10:40:24 +0300

    OBG-007: Moonshot project created

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ozan Barış Günaydın on 28.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
