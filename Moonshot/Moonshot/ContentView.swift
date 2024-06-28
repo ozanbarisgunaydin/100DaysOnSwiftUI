493a22645643de95a7c61d035a47f8db535d4ade
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-28 10:58:30 +0300

    OBG-007: Scroll view and Lazy Stacks

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new Custom Text")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Image(.example)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) { /// Loads items if needed with `Lazy` but the size expands and it may create some UI issues
+                ForEach(0..<100) {
+                    CustomText(text: "Item of \($0)")
+                        .font(.title)
+                }
             }
+        }
     }
 }
 
