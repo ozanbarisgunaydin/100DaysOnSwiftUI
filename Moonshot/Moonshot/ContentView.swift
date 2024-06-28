a20fe18f80fbe2bb1b00ada67ca86a46c0a59d62
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-28 12:44:56 +0300

    OBG-007: H and V Grids

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,35 +7,28 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
 struct ContentView: View {
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Aenue",
-                    "city": "Nashville"
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout, content: {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+                    
                 }
-            }
-            """
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            
-            if let user = try? decoder.decode(User.self, from: data) {
-                print("⭕️ \(user.address.street)")
-            }
+            })
+        }
+
+        ScrollView {
+            LazyVGrid(columns: layout, content: {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+
+                }
+            })
         }
     }
 }
