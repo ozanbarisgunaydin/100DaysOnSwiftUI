66d66b948e018f4dc11b1cc817163c73522fe11d
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-28 18:41:16 +0300

    OBG-007: Astronaut view

---
+++ Moonshot/Moonshot/Views/AstronautView.swift
@@ -0,0 +1,34 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Ozan Barış Günaydın on 28.06.2024.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+                    .foregroundColor(.white)
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    return AstronautView(astronaut: astronauts["aldrin"] ?? Astronaut(id: "", name: "", description: ""))
+}
