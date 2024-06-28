d789d167a4863375cf306e3cd8c0fc93fa415c8e
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-06-28 17:45:44 +0300

    OBG-007: Detail view handled

---
+++ Moonshot/Moonshot/Views/MissionView.swift
@@ -0,0 +1,44 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Ozan Barış Günaydın on 28.06.2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
