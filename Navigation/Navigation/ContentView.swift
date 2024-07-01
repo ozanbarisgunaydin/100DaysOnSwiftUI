b22ac731b5bb1c3b5ade02a4bfddadc8ab6d0f60
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-01 18:37:49 +0300

    OBG-008: Going root view

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,34 +7,30 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
+    }
+}
+
 struct ContentView: View {
-    @State private var path = NavigationPath() /// Type eraser for navigation flows
+    @State private var path = NavigationPath()
     
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
                 }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
-            .toolbar {
-                Button("Push 556") {
-                    path.append(556)
-                }
-                Button("Push Hello") {
-                    path.append("Hello")
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the text \(selection)")
-            }
         }
     }
 }
