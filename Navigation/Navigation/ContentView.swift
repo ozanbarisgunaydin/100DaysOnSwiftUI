860c05cd7c9d26a8daf5fc4a12ef630dbe57dd65
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-01 18:27:39 +0300

    OBG-008: Type depended route

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -8,25 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var path: [Int] = []
     
     var body: some View {
-        NavigationStack(path: $path) {
-            VStack {
-                Button("Show 32") {
-                    path = [32]
+        NavigationStack {
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
                 }
                 
-                Button("Show 64") {
-                    path = [64]
-                }
-                
-                Button("Show 32 than 64") {
-                    path = [32, 64]
+                ForEach(0..<5) { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
                 }
             }
             .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the text \(selection)")
             }
         }
     }
