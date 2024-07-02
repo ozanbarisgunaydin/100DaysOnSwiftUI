818f53e22157bb94ab6033ba6b41231947a89232
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-02 11:37:54 +0300

    OBG-009: New project created: Cupcake Corner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ozan Barış Günaydın on 2.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
