b8f791b3a81f4c7c0a25d9d5d51f58018bd1028e
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-04 12:34:35 +0300

    OBG-011: New Project created

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Ozan Barış Günaydın on 4.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Ozan Barış Günaydın on 4.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
