be57f1608a1303dafae2144e46c23c08d30107a3
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-05 11:03:54 +0300

    OBG-012: Filter applying

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Ozan Barış Günaydın on 4.07.2024.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
 
@@ -12,6 +14,9 @@ struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     @State private var selectedItem: PhotosPickerItem?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
 
     var body: some View {
         NavigationStack {
@@ -39,6 +44,7 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
                 }
                 
                 HStack {
@@ -60,9 +66,20 @@ struct ContentView: View {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self),
                   let inputImage = UIImage(data: imageData) else { return }
             
-            // more code
-            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage,
+              let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 
 }
