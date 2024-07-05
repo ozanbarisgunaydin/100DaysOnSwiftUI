44041030d486f4fb65e72fd6d2a137387c822c5f
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-05 16:40:57 +0300

    OBG-012: Bucket List app created

---
+++ Bucket List/Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Ozan Barış Günaydın on 5.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
