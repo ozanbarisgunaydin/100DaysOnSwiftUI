31ffb3273dc33a9b3b5d21593b3277d0156ed1bd
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-05 10:44:32 +0300

    OBG-012: Basic layout added

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView(
+                        "No picture",
+                        systemImage: "photo.badge.plus",
+                        description: Text("Tap to import a photo")
+                    )
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
     }
+    
+    func changeFilter() {
+        
+    }
+
 }
 
 #Preview {
