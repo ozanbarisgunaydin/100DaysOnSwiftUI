2fb17f237fbbf8cd2cd44e7ebf595167f53f5d7a
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-05 10:38:19 +0300

    OBG-011: BlurView added

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,24 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
-    
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-            
-            /// Changes property value then the didSet of the blurAmount is triggered:
-            /// Button("Random Blur") {
-            ///     blurAmount = Double.random(in: 0...20)
-            /// }
-            ///
-            /// Only changes the current value not the wrapper value for fetching the changes we need to fetch the onChange block
-            Slider(value: $blurAmount, in: 0...20)
-        }
-        .onChange(of: blurAmount) { oldValue, newValue in
-            print("⭕️ \(newValue)")
-        }
+        Text("Hello, World!")
     }
 }
 
