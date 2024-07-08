bd47a12c0a6884c4965cec3e978ed9895ed74a9e
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-08 18:42:51 +0300

    OBG-013: Sorted with comporables

--- Bucket List/Bucket List/Views/EditView.swift
+++ Bucket List/Bucket List/Views/EditView.swift
@@ -43,7 +43,7 @@ struct EditView: View {
                             
                             + Text(": ") +
                             
-                            Text("Page description here")
+                            Text(page.description)
                                 .italic()
                         }
                         
@@ -92,10 +92,11 @@ struct EditView: View {
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             loadingState = .failed
+            print("⭕️ Error on loading: \(error.localizedDescription)")
         }
     }
 }
