c73a70cb3a39e004f91cd68cab0e42f0a3deb39e
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-08 18:09:33 +0300

    OBG-013: New locations

--- Bucket List/Bucket List/OtherLessonMaterials/Location.swift
+++ Bucket List/Bucket List/OtherLessonMaterials/Location.swift
@@ -6,11 +6,39 @@
 //
 
 import Foundation
+import MapKit
 
 struct Location: Codable, Equatable, Identifiable {
-    let id: UUID
+    // MARK: - Identifiable
+    var id: UUID /// Since the location name and desc can be edited UUID must be changed
+    
+    // MARK: - Parameters
     var name: String
     var description: String
     var latitude: Double
     var longitude: Double
+    
+    // MARK: - Computed Properties
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: latitude,
+            longitude: longitude
+        )
+    }
+    
+    // MARK: - Samples
+#if DEBUG
+    static let example = Location(
+        id: UUID(),
+        name: "Buckingham Palace",
+        description: "Lit by over 40,000",
+        latitude: 41.501,
+        longitude: -0.141
+    )
+#endif
+    
+    // MARK: - Equatable
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
--- Bucket List/Bucket List/OtherLessonMaterials/MapMarkers.swift
+++ Bucket List/Bucket List/OtherLessonMaterials/MapMarkers.swift
@@ -17,18 +17,26 @@ struct MapMarkers: View {
     )
     
     @State private var locations: [Location] = []
+    @State private var selectedPlace: Location?
     
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
                 ForEach(locations) { location in
-                    Marker(
+                    Annotation(
                         location.name,
-                        coordinate: CLLocationCoordinate2D(
-                            latitude: location.latitude,
-                            longitude: location.longitude
-                        )
-                    )
+                        coordinate: location.coordinate
+                    ) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
+                    }
                 }
             }
             .onTapGesture { position in
@@ -42,6 +50,14 @@ struct MapMarkers: View {
                         longitude: coordinate.longitude
                     )
                     locations.append(newLocation)
+                }
+            }
+            
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
                 }
             }
         }
