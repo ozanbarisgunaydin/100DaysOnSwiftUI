5ebe3d4e500bbcd3984d9c73b3e4ac823e90629e
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-19 23:48:20 +0300

    OBG-015: Accessibility project created with assets

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Ozan Barış Günaydın on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
