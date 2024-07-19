1ab957f516379a3c62ec9f16db67ca7a1c41de2e
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-19 23:58:47 +0300

    OBG-015: HotProspects project created

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Ozan Barış Günaydın on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
