5b88c05a9b61c9c2912b0712879774fe81055d27
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-20 02:21:44 +0300

    OBG-015: View is refreshed for real project files

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,19 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
-    @State private var selection = Set<String>()
 
     var body: some View {
-        List(users, id: \.self, selection: $selection) { user in
-                Text(user)
-        }
-        
-        if !selection.isEmpty {
-            Text("You selected \(selection.formatted())")
-        }
-        
-        EditButton()
+        Text("Hello, Ezgi!")
     }
 }
 
