d4fa347bf6d40f25ad042f3933b97ec54ac170b8
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-24 18:44:43 +0300

    OBG-016: Generte QR code

--- HotProspects/HotProspects/Views/MeView.swift
+++ HotProspects/HotProspects/Views/MeView.swift
@@ -5,11 +5,48 @@
 //  Created by Ozan Barış Günaydın on 24.07.2024.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @AppStorage("name") private var name = "Anonymous"
+    @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title2)
+                
+                TextField("Email Address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title2)
+                
+                Image(uiImage: generateQRCode(from: "\(name)_\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        guard
+            let outputImage = filter.outputImage,
+            let cgImage = context.createCGImage(outputImage, from: outputImage.extent)
+        else {
+            return UIImage(systemName: "xmark.circle") ?? UIImage()
+        }
+        
+        return UIImage(cgImage: cgImage)
     }
 }
 
