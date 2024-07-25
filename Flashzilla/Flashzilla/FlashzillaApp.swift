13783f30b4346562ba3dc4369aab81023b0df6c1
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-25 11:10:20 +0300

    OBG-016: Flashzilla app created

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Ozan Barış Günaydın on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
