e8891e25f7f709c76ebab2bfa59add6fb8e5b6f4
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-07-30 18:13:26 +0300

    OBG-018: Card remove animation

--- Flashzilla/Flashzilla/Views/CardView.swift
+++ Flashzilla/Flashzilla/Views/CardView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct CardView: View {
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    
     let card: Card
+    var removal: (() -> Void)? = nil
     
     var body: some View {
         ZStack {
@@ -32,6 +35,24 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(
+            .degrees(Double(offset.width / 5.0))
+        )
+        .offset(x: offset.width * 5)
+        .opacity(2 - Double(abs(offset.width / 50.0)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
