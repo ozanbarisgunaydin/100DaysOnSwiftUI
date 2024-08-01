8a2f35db4641c29f8f025fd169550e07eb4df7d0
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-02 00:57:08 +0300

    OBG-019: Modifier order matters

---
+++ LayoutAndGeometry/LayoutAndGeometry/OtherStuffs/AbsolutePositioning.swift
@@ -0,0 +1,20 @@
+//
+//  AbsolutePositioning.swift
+//  LayoutAndGeometry
+//
+//  Created by Ozan Barış Günaydın on 2.08.2024.
+//
+
+import SwiftUI
+
+struct AbsolutePositioning: View {
+    var body: some View {
+        Text("Hello, World!")
+            .offset(x: 100, y: 100)
+            .background(.red)
+    }
+}
+
+#Preview {
+    AbsolutePositioning()
+}
