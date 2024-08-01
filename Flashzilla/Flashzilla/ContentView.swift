348f4d9156a388145ca2ac9f9cc3e01407ccb105
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-01 11:53:35 +0300

    OBG-019: Start again logic

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -50,6 +50,16 @@ struct ContentView: View {
                         .stacked(at: index, in: cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                        .padding(20)
+                }
             }
             
             if accessibilityDifferentiateWithoutColor {
@@ -84,12 +94,28 @@ struct ContentView: View {
             }
         }
         .onChange(of: scenePhase) {
-            isActive = scenePhase == .active
+            if scenePhase == .active {
+                if cards.isEmpty == false {
+                    isActive = true
+                }
+            } else {
+                isActive = false
+            }
         }
     }
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
