98f025dbf8acb28c102578841e3fc883dd7591cb
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-02 01:41:36 +0300

    OBG-019: SnowSeeker project created

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Ozan Barış Günaydın on 2.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
