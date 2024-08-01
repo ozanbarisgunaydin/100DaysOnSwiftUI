6a366ca56e399bdb6dca6a5ee1226c3a7c9b5ab6
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-02 00:45:39 +0300

    OBG-019: Other stuffs created

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,40 +7,9 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    struct MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { dimension  in
-                        dimension[VerticalAlignment.center]
-                    }
-                
-                Image(.paulHudson)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { dimension  in
-                        dimension[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-
-            }
-        }
+        Text("Hello, world!")
     }
 }
 
---
+++ LayoutAndGeometry/LayoutAndGeometry/OtherStuffs/CustomAlignment.swift
@@ -0,0 +1,49 @@
+//
+//  CustomAlignment.swift
+//  LayoutAndGeometry
+//
+//  Created by Ozan Barış Günaydın on 2.08.2024.
+//
+
+import SwiftUI
+
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct CustomAlignment: View {
+    var body: some View {
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAccountAndName) { dimension  in
+                        dimension[VerticalAlignment.center]
+                    }
+                
+                Image(.paulHudson)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAccountAndName) { dimension  in
+                        dimension[VerticalAlignment.center]
+                    }
+                    .font(.largeTitle)
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    CustomAlignment()
+}
