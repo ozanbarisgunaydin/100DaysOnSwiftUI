48ef5b05422db8b2825f7674ebdf8ca119420593
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-01 12:35:16 +0300

    OBG-019: New project created

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Ozan Barış Günaydın on 1.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
