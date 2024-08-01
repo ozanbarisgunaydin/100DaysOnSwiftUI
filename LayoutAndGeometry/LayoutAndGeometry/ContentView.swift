3c9fcf47bf6cf034d8d2138cc0a4b38b49e828b6
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-01 15:12:09 +0300

    OBG-019: Basic toughts

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, world!")
+            .background(.red)
+            .padding(20)
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.blue, lineWidth: 2)
+            }
     }
 }
 
