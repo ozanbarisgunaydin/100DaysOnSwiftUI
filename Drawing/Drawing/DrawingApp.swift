96755b0ceb9ce22dd7589f0554f7f9d117edbed2
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-05 17:15:55 +0300

    OBG-020: Bonus project - Drawing

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ozan Barış Günaydın on 5.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
