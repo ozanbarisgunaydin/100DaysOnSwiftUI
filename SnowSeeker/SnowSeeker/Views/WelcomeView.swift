3765ec555764e4144440f054d2cbdc9e1800e434
Ozan Barış Günaydın <ozangunaydin61@gmail.com>
2024-08-05 16:06:31 +0300

    OBG-020: Welcome view

---
+++ SnowSeeker/SnowSeeker/Views/WelcomeView.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Ozan Barış Günaydın on 5.08.2024.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SnowSeeker")
+                .font(.largeTitle)
+            Text("Please select a resort from the left-hand menu; swipe from the left edge to show it.")
+            .foregroundStyle(.secondary)
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
